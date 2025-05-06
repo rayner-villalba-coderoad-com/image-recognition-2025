e84ffe83252d652bb2695fb4836b9a61ae20d17e
rvillalba <rayner.villalba@mojix.com>
2025-05-06 09:15:29 -0400

    Agregando lambda function

--- serverless-image-app/handler.js
+++ serverless-image-app/handler.js
@@ -1,8 +1,24 @@
-exports.hello = async (event) => {
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+module.exports.getSignedUrl = async (event) => {
+  const { filename, filetype } = event.queryStringParameters;
+  const key = `uploads/${Date.now()}-${filename}`;
+
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: key,
+    Expires: 60, // tiempo en segundos
+    ContentType: filetype,
+  };
+
+  const uploadURL = await s3.getSignedUrlPromise('putObject', params);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
+    body: JSON.stringify({ uploadURL, key }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   };
 };
